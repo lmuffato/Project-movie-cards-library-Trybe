a4063221dfc1660f6b49480fb81d22c11dd3b2be
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-04-28 18:22:38 -0300

    Adicionando requisito 4 e dando commit pra fazer pull de correção

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,27 @@
-// implement MovieCard component here
+import React from 'react';
+
+function MovieCard(props) {
+
+  return (
+    <div className="movie-card">
+      <img src={props.movie.imagePath} alt={`foto ${props.movie.title}`} />
+      <h2>{ props.movie.title }</h2>
+      <h3>{ props.movie.subtitle }</h3>
+      <p>{ props.movie.storyline }</p>
+    </div>
+  );
+}
+
+// function MovieCard(props) {
+//   const { movieInfo: { imagePath, title, subtitle, storyline } } = props;
+//   return (
+//     <div className="movie-card">
+//     <img src={ imagePath } alt={ `foto ${title}` } />
+//     <h2>{ title }</h2>
+//     <h3>{ subtitle }</h3>
+//     <p>{ storyline }</p>
+//     </div>
+//   );
+// }
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 function MovieList(props) {
+  const { movies } = props;
   return (
-    <section>
-      {props}
+    <section className="movie-list">
+      {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
     </section>
   );
 }
