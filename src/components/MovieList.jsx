9e9c7a4e5e823139344a3ed2996f804d2c5b29df
guilhermemd <guilherme.md@gmail.com>
2021-04-28 18:08:23 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieList() {
+  return (
+    <section>
+      <MovieCard />
+    </section>
+  );
+}
+
+export default MovieList;
