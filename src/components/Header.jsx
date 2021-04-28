17f76b0a94cb9099db92994a2bc9c49ed0b289f0
diegoCorreia <diegomethal@gmail.com>
2021-04-28 18:32:20 -0300

    requisitos 1, 2, 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
+import React from 'react';
 // implement Header component here
+function Header(){
+    return (<header>
+        <h1>Movie Cards Library</h1>
+    </header>)
+}
+
+export default Header
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+
+const {title, subtitle, storyline, rating, imagePath} = movies;
+
+function MovieList(prop) {
+    return ( <h1> ola </h1>
+        
+    //     {movies = {
+    //     title: prop.title,
+    //     subtitle: prop.subtitle,
+    //     storyline: prop.storyline,
+    //     rating: prop.rating,
+    //     imagePath: prop.imagePath
+    //  } }
+)
+}
+
+export default MovieList
