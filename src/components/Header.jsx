0efa82d0d61b952cfdf4283e42c5da3c798f464c
Iago de Paula <iago.pferreiravr@gmail.com>
2021-04-28 15:05:40 -0300

    Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,10 @@
 // implement Header component here
-// Iniciando o projeto
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return ('');
+  }
+}
+
+export default Header;
