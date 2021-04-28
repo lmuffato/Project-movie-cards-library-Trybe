df76ab2a6aa9e5dc5b5c3aa00a0cf6757edc62dd
Iago de Paula <iago.pferreiravr@gmail.com>
2021-04-28 15:28:14 -0300

    Requisito 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,6 +7,7 @@ class MovieCard extends React.Component {
       <div>
         <img src={this.props.movie.imagePath} alt={`${this.props.movie.title} poster`} />
         <h4>{this.props.movie.title}</h4>
+        <h5>{this.props.movie.subtitle}</h5>
       </div>
     );
   }
