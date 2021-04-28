06962ff1edbb5de9a4aea264113ace678a1b87d4
Rafael Cardoso <rafael.vimur@gmail.com>
2021-04-28 17:43:59 -0300

    finalização do projeto

---
+++ src/components/Rating.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Rating({ rating }) {
+  return <span className="rating">{rating}</span>;
+}
+
+Rating.propTypes = { rating: PropTypes.number }.isRequired;
+
+export default Rating;
