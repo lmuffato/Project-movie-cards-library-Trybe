d754c65b09c1ab9c81af321ccf8715c38bb610c9
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-04-28 18:45:53 -0300

    Adicionando requisito 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,14 +5,14 @@ import Rating from './Rating';
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <fieldset>
         <img src={ imagePath } alt="Imagem do filme" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <Rating />
+        <Rating rating={ rating } />
       </fieldset>
     );
   }
