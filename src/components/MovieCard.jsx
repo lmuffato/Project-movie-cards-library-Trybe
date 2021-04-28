7ec8592bb59ee8b3c1caf61a1ed88553be32acf5
Mateus Alencar <mtauws@gmail.com>
2021-04-28 16:26:07 -0300

    chore: requirement 4:lint p.2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,1 @@
 // implement MovieCard component here
-import React from 'react';
-
-class MovieCard extends React.Component {
-  render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
-    return (
-      <div className="movie-list">
-        { movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />) }
-      </div>
-    );
-  }
-}
-
-export default MovieCard;
