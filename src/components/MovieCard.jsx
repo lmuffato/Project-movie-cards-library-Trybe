cc4954637f3fc9ce3218463f5934983c50e3ebbc
Rafael Ribeiro <rafittu@outlook.com>
2021-04-28 17:09:53 -0300

    correcao lint no rating.js

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ class MovieCard extends React.Component {
 
     return (
       <div>
-        <img src={ imagePath } alt={title} />
+        <img src={ imagePath } alt={ title } />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
@@ -9,5 +10,9 @@ class Rating extends React.Component {
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
 
 export default Rating;
