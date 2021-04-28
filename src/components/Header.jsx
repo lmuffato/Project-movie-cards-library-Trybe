31ce59c4ad5a99930578a4b7b88500f05a258fea
guidugaich <guidugaich.dev@gmail.com>
2021-04-28 17:09:17 -0300

    requisitos 1 e 2 - renderizando elemento Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// Iniciando o projeto 🚀
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Movie Cards Library</h1>
+    </header>
+  );
+}
+
+export default Header;
