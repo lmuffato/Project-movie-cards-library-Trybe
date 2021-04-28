bcb76adfb31dd0024ee1832768bef0e05c71f1dd
Lucas André <lucas.cancado89@gmail.com>
2021-04-28 15:27:24 -0300

    Iniciando VQV

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+
+function MovieCard(prop) {
+  const { title, subtitle, storyline, imagePath, rating} = prop.movie;
+  return (
+    <div>
+      <img src={ imagePath } alt="imagem do filme" />
+      <h4>{title}</h4>
+      <h5>{subtitle}</h5>
+      <p>{storyline}</p>
+    </div>
+  )
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieList(prop) {
+  return (
+    prop.movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+  );
+}
+
+export default MovieList;
