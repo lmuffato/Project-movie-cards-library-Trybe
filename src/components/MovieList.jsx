3eb816d9f3d525381bfb174736137bffeb530f34
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-28 16:38:09 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+// Requisito 3
+import React from 'react';
+
+function MovieList(props) {
+  return (
+    <main>{props}</main>
+  );
+}
+
+export default MovieList;
