bc08030bdbfb3dcc3c64e8e0c3cbed70c484366d
Edrei <edreimateus@gmail.com>
2021-04-28 17:30:27 -0300

    fix issues

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
+
+    return (
+      <div>
+        <div>
+          <h1>{title}</h1>
+          <h1>{subtitle}</h1>
+          <p>{storyline}</p>
+        </div>
+        <img src={ imagePath } alt={ `${title}` } />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
