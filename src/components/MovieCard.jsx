d04cc8fc194d46d7f9a20539f643308787a6ceaf
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-04-28 18:33:55 -0300

    Requirement 6 and 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props
     return (
-      <div></div>
+        <img src={movie.imagePath} alt='image'></img>
     )
   }
 }
