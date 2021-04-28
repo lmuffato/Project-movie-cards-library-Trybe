4d4893362096e0a22a08b5682118b62d4b07b8ff
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-04-28 13:49:14 -0300

    espacos em branco retirados

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,9 +8,9 @@ class MovieCard extends React.Component {
     return (
         <div>
             <img src={ imagePath } alt={ title } />
-            <h4> { title } </h4>
-            <h5> { subtitle } </h5>
-            <p> { storyline } </p>
+            <h4>{ title }</h4>
+            <h5>{ subtitle }</h5>
+            <p>{ storyline }</p>
         </div>
       )
   }
