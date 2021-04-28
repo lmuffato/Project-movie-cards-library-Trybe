48d844a967b78005b369782fe52ee80d547b35e0
Juan Pablo Martins <juanpm@pop-os.localdomain>
2021-04-28 18:16:14 -0300

    Bora e bora

---
+++ .history/src/components/Header_20210428180940.jsx
@@ -0,0 +1,7 @@
+import React from react
+
+class Header extends react.Components {
+  render() {
+    return (<h1>Movie Cards Library</h1>)
+  }
+}
---
+++ .history/src/components/Header_20210428181200.jsx
@@ -0,0 +1,9 @@
+import React from React
+
+class Header extends react.Components {
+  render() {
+    return (<header>
+      <h1>Movie Cards Library</h1>
+      </header>)
+  }
+}
