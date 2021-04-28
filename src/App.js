0635b379986a9a3eb263c0de2869171761c3f2fa
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-28 14:40:19 -0300

    [REQUISITO][01, 02]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+
+import data from './data';
 import './App.css';
 
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+// import Rating from './components/Rating';
+
 function App() {
+  // const ratings = data.map(({ rating }) => rating);
+
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />
+      <MovieList movies={ data } />
+      {/* <Rating ratings={ ratings } /> */}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    // Declaração do objeto contendo informações do album01
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
