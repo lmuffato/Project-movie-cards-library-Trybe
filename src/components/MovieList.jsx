fd0a427a72eca036fd6b90e8027b22df4cae973d
Felipe Muller <felipeavmuller@gmail.com>
2021-04-28 20:07:19 -0300

    Adicionado css

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,11 +7,13 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      movies.map((movie) => (
-        <MovieCard
-          key={ movie.title }
-          movie={ movie }
-        />))
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard
+            key={ movie.title }
+            movie={ movie }
+          />))}
+      </div>
     );
   }
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,11 @@ import PropTypes from 'prop-types';
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
-    return <span className="rating">{rating}</span>;
+    return (
+      <div className="movie-card-rating">
+        <span className="rating">{rating}</span>
+      </div>
+    );
   }
 }
 
