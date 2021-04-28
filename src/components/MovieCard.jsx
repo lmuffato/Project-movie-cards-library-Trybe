143317f614958caa568ecf74248c865322fe3fa4
Orlando Flores <orlando_flores@id.uff.br>
2021-04-28 15:39:35 -0300

    requisitos 3, 4 e 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div> 
+        coe
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } movies={ movie } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
