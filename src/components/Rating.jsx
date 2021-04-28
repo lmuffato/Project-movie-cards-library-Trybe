db8f51fb5c7935fdf3bd3e7cf9926e89e00079bd
Robertson <robertson.nunes@gmail.com>
2021-04-28 20:52:48 -0300

    Commit BOMBA Requisitos 12, 13 e 14

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
-  render () {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <span className="rating">{ rating }</span>
+      </div>
+    );
   }
 }
 
-export default Rating
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
