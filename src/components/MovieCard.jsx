1673d6a0092101b71a7b294d3248b6ce8d8c9d3c
Gabriel Ferreira <glucas709@gmail.com>
2021-04-28 20:34:51 -0300

    Requisito 6 ao 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,8 +5,8 @@ const MovieCard = ({ movie }) => {
   const { title, subtitle, storyline, imagePath } = movie;
   return (
     <div>
-      <h3>{title}</h3>
-      <span>{subtitle}</span>
+      <h4>{title}</h4>
+      <h5>{subtitle}</h5>
       <p>{storyline}</p>
       <img src={ imagePath } alt="Capa do filme" />
     </div>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-const MovieList = ({ movies }) => {
-  console.log(movies)
-  return (
-    <section>
-      {movies.map((movie) => (
-        <MovieCard key={ movie.title } movie={ movie } />
-      ))}
-    </section>
-  );
-};
+const MovieList = ({ movies }) => (
+  <section>
+    {movies.map((movie) => (
+      <MovieCard key={ movie.title } movie={ movie } />
+    ))}
+  </section>
+);
 
 MovieList.propTypes = {
   movies: PropTypes.arrayOf(PropTypes.shape({
