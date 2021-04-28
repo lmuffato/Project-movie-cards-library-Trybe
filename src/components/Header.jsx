698b636adaa59aca110c2b33f6e8123fd6412118
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-28 15:49:57 -0300

    Requisitos 1 e 2 corrige lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    const h1 = <h1>Movie Cards Library</h1>
-    const header = <header>{h1}</header>
+    const h1 = <h1>Movie Cards Library</h1>;
+    const header = <header>{h1}</header>;
     return (
-      header
-  );
-};
-
+        header
+        );
+    };
 }
 
 export default Header;
