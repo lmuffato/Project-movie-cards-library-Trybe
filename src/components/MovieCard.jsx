bda98e2a30037b096a97263c9579fd4585ae875c
Marcelo Maurício <mmauricio.design@gmail.com>
2021-04-28 15:54:58 -0300

    Requisito 4 e 5 - criacao e rederizacao do componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
 // implement MovieCard component here
+import React from 'react';
+
+function MovieCard(movie) {
+  const { title } = movie;
+  return (
+    <h1>{ title }</h1>
+  );
+}
+
+export default MovieCard;
