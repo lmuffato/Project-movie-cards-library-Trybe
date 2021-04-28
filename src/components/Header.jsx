3ca11712ffb1fd6f9e7180ed167788a1d22b2dd0
madruga665 <madruga665@hotmail.com>
2021-04-28 16:15:00 -0300

    Requisitos 3 e 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <h1>Movie Cards Library</h1>;
+    return <header><h1>Movie Cards Library</h1></header>;
   }
 }
 
