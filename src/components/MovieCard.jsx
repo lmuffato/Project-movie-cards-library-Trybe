bee752db5d1ad75714741e65e32ddab71e5d2625
Robertson <robertson.nunes@gmail.com>
2021-04-28 20:41:27 -0300

    Requisito 11

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating'
 
 class MovieCard extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
+        <Rating />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,8 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render () {
+  }
+}
+
+export default Rating
