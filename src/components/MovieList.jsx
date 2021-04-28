7b3cbfe7a32e2087b9d2de95dba982b6b10d505a
Mateus Alencar <mtauws@gmail.com>
2021-04-28 15:51:31 -0300

    chore: requirement 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+// import MovieCard from './MovieCard';
+class MovieList extends React.Component {
+  render() {
+    // const { movies } = this.props;
+    return (
+      <section id="movie-list">
+      </section>
+    );
+  }
+}
+
+export default MovieList;
