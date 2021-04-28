924ad9c82163797a087c6da734757245be83c48e
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-04-28 14:28:48 -0400

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+// requisito 1
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
