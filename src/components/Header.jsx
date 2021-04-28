3a13fe3359e1a03d3cd39fff242445763bdd0996
Felipe Muller <felipeavmuller@gmail.com>
2021-04-28 16:03:02 -0300

    Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,10 @@
-// implement Header component her
+// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return;
+  }
+}
+
+export default Header;
