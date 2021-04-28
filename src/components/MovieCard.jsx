5bf338ee130f553dee858df85394d8b0df6575dd
Patrick Morais <ppternunes@gmail.com>
2021-04-28 15:41:08 -0300

    requisito 6 e 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
-// implement MovieCard component here
+import React from 'react';
+
+function MovieCard(props) {
+  return (
+    <>
+      <img src={ props.movieProp.imagePath } alt='Movie Card'></img>
+      <h4>{ props.movieProp.title }</h4>
+    </>
+  );
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 function MovieList(props) {
-  return (<>
-    {props.movies.map((movie) => <MovieCard key={ movie.title } />)}
-          </>
+  return (
+    <>
+      {props.movies.map((movie) => <MovieCard key={ movie.title } movieProp={ movie } />)}
+    </>
   );
 }
 
