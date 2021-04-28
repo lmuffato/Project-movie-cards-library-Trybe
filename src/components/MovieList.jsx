9a79c0db9769dd896754c6b2b65e4a004008b077
Rafael Ribeiro <rafittu@outlook.com>
2021-04-28 16:29:44 -0300

    requisitos 4 ao 10

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import MovieCard from "./MovieCard";
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+
+    return (
+      movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)
+    );
   }
 }
 
