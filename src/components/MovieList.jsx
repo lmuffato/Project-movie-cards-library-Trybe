fd81f12131b8d34030dbd6f5af186f18fe04ccc5
lucas-paz <lucas.hpaz@outlook.com>
2021-04-28 16:34:56 -0300

    Requisitos 1 ao 12

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie, index) => <MovieCard movie={ movie } key={ index } />)}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.Object).isRequired,
+};
+
+export default MovieList;
