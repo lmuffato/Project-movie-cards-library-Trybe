b89d67ca5d5e15a95dd7b4e0a1429c9c7febc0ac
madruga665 <madruga665@hotmail.com>
2021-04-28 17:47:25 -0300

    Requisito 9 agora ta fluindo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,21 +4,23 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title } = movie;
+    const { imagePath, title, subtitle } = movie;
     return (
       <div>
         <img src={ imagePath } alt="foto do filme" />
         <h4>{title}</h4>
+        <h5>{subtitle}</h5>
       </div>
     );
   }
 }
 
-// referencia sobre proptypes array com shape https://stackoverflow.com/questions/32325912/react-proptype-array-with-shape
+// referencia sobre proptypes array https://stackoverflow.com/questions/32325912/react-proptype-array-with-shape
 MovieCard.propTypes = {
   movie: PropTypes.arrayOf(PropTypes.shape({
     imagePath: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
   })).isRequired,
 };
 export default MovieCard;
