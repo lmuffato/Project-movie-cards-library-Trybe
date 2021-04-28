e1f9c4899d507630c101a5a35b57ac11a10ec92b
Iago de Paula <iago.pferreiravr@gmail.com>
2021-04-28 15:27:46 -0300

    Requisito 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,10 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     return (
-      <img src={this.props.movie.imagePath} alt={`${this.props.movie.title} poster`} />
+      <div>
+        <img src={this.props.movie.imagePath} alt={`${this.props.movie.title} poster`} />
+        <h4>{this.props.movie.title}</h4>
+      </div>
     );
   }
 }
