ab27ccd735454cd9b46623f638ae721b2aa3b3b6
Iago de Paula <iago.pferreiravr@gmail.com>
2021-04-28 15:19:06 -0300

    Requisito 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return ('');
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,19 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return ('');
+    const { movies } = this.props;
+    // console.log(this.props);
+    return (
+      <div>
+        {movies.map((movie) => (
+          <MovieCard />
+        ))}
+      </div>
+    );
   }
 }
 
