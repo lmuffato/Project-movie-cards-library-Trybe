386a23e25672bdad6860a2c883bd24154bf5d38f
Mateus Alencar <mtauws@gmail.com>
2021-04-28 16:55:48 -0300

    chore: comment in MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,11 +8,12 @@ class MovieCard extends Component {
     const { title, subtitle, storyline, imagePath, rating } = movie;
 
     return (
+      // https://github.com/tryber/sd-09-project-movie-cards-library/tree/2ca7196a9e082d5e716dbfc0b6d50cc2c64938e9
       <div className="movie-card">
         <div className="movie-card-body">
           <img className="movie-card-image" src={ imagePath } alt={ title } />
           <h4 className="movie-card-title">{ title }</h4>
-          <h5 className="movie-card-subtitle">{ subtitle}</h5>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
           <Rating className="movie-card-rating" rating={ rating } />
         </div>
@@ -23,11 +24,11 @@ class MovieCard extends Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired,
-    imagePath: PropTypes.string.isRequired,
-    storyline: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    imagePath: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
