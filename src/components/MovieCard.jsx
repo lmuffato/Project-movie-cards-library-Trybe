56d2310b67a04447cc8150a37d898dab6ef989d1
Gabriel Ferreira <glucas709@gmail.com>
2021-04-28 20:29:06 -0300

    Requisito 4 e 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,25 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MovieCard = ({ movie }) => {
+  const { title, subtitle, storyline, imagePath } = movie;
+  return (
+    <div>
+      <h3>{title}</h3>
+      <span>{subtitle}</span>
+      <p>{storyline}</p>
+      <img src={ imagePath } alt="Capa do filme" />
+    </div>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 const MovieList = ({ movies }) => {
   console.log(movies)
   return (
     <section>
+      {movies.map((movie) => (
+        <MovieCard key={ movie.title } movie={ movie } />
+      ))}
     </section>
   );
+};
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  })).isRequired,
 };
 
 export default MovieList;
