706cd3fd176ebbfe34a33870584f469ccdeee069
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-04-28 18:06:25 -0300

    Requirement 3 and 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    <div></div>
+  }
+}
+
+export default MovieCard
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieList(props) {
+  return (
+    <div>
+      {props.movies.map((movie) => <MovieCard movie={movie} />)}
+    </div>
+  )
+}
+
+export default MovieList
