c303cf3ce497433596a820f0e5305032092b1b8e
Viegas <mvrdgs@gmail.com>
2021-04-28 17:07:27 -0300

    requisitos 6 a 10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={[{title: 'a'}]} />
+      <MovieList movies={[{title: 'Avatar',
+      imagePath: 'https://m.media-amazon.com/images/M/MV5BODc5YTBhMTItMjhkNi00ZTIxLWI0YjAtNTZmOTY0YjRlZGQ0XkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_UX182_CR0,0,182,268_AL_.jpg',
+      subtitle: 'A Lenda de Aang ',
+      storyline: 'In a war-torn world of elemental magic, a young boy reawakens to undertake a dangerous mystic quest to fulfill his destiny as the Avatar, and bring peace to the world.',
+      rating: 9.2,
+      }]} />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const {
+      title,
+      subtitle,
+      storyline,
+      imagePath,
+      rating
+    } = this.props.movie
+    
     return (
       <div>
-        'test'
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <img src={imagePath} />
+        {/* <Rating rating={ rating } /> */}
       </div>
     );
   };
