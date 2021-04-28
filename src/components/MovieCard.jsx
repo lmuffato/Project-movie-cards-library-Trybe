fa585acd40aee96adf98a6cca593c9f6f87b6e23
andy silva <andssilva.23@gmail.com>
2021-04-28 17:05:54 -0400

    feat(requisito 8 e 9): :rocket: Renderiza título do filme num h4, Renderiza subtítulo num h5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,7 +10,8 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt="movie" />
-        
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
       </div>
     );
   }
