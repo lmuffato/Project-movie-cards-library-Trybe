12861a7bdbfd9ef4b8015b4a6579f59c62845a63
defauth98 <neto.daniribeiro@gmail.com>
2021-04-28 16:11:01 -0300

    Fix eslint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import MovieCard from './MovieCard'
 
 function MovieList(props) {
-  const movies = props.movies
+  const movies = props.movies;
 
   return (
     <div>
-      {movies.map(movie => <MovieCard key={movie.title}/>)}
+      {movies.map(movie => <MovieCard key={ movie.title }/>)}
     </div>
-  )
-};
+  );
+}
 
 export default MovieList;
