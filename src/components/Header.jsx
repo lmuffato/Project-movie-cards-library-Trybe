2289eaa90cea2f5937fcdf024a2862093c4f2670
Mateus Alencar <mtauws@gmail.com>
2021-04-28 15:21:45 -0300

    chore: requirement 1 and 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,14 @@
 // implement Header component here
-"intial commit" 
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>Movie Cards Library</h1>
+            </header>
+        );
+    }
+}
+
+export default Header;
