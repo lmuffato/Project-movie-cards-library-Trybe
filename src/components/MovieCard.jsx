ebcee74c80b6378406465c76d0ca9e1f361fb8e9
madruga665 <madruga665@hotmail.com>
2021-04-28 18:05:50 -0300

    Requisito 12, 13 e 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline } = movie;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
     return (
       <div>
         <img src={ imagePath } alt="foto do filme" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -23,6 +25,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
+    rating: PropTypes.number,
   })).isRequired,
 };
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return (<p>rating</p>);
+    const { rating } = this.props;
+    return (rating);
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
