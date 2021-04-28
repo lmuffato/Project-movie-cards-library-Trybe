3957be281a832102ef8241df2e88066fc90a6e48
Orlando Flores <orlando_flores@id.uff.br>
2021-04-28 16:11:49 -0300

    requisitos 6, 7, 8, 9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,10 @@ class MovieCard extends Component {
     const { movie } = this.props;
     return (
       <div>
-        { movie.title }
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
+        <img src={ movie.imagePath } alt={ movie.title } />
       </div>
     );
   }
