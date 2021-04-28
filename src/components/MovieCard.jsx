8acb88bfd126e1faf64902ad903280adb88e9c49
Robertson <robertson.nunes@gmail.com>
2021-04-28 20:33:33 -0300

    Commit BOMBA Requisitos 7 a 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle } = movie;
+    const { imagePath, title, subtitle, storyline } = movie;
     return (
       <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
       </div>
     );
   }
@@ -16,8 +18,10 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    imagePath: PropTypes.string,
     title: PropTypes.string,
     subtitle: PropTypes.string,
+    storyline: PropTypes.string,
   }).isRequired,
 };
 
