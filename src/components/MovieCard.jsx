582aa32f2aa46bafb4d4ba0f2d323a00c70cbbeb
Guilherme de Prais <guilherme.prais@gmail.com>
2021-04-28 19:28:49 -0300

    MovieCard: div correction

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,9 +15,9 @@ function MovieCard({ movie }) {
         <h4 className="movie-card-title">{ movie.title }</h4>
         <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
         <p className="movie-card-storyline">{ movie.storyline }</p>
-        <div className="movie-card-rating">
-          <Rating rating={ movie.rating } />
-        </div>
+      </div>
+      <div className="movie-card-rating">
+        <Rating rating={ movie.rating } />
       </div>
     </div>
   );
