40af5b3d4f21dbf07589358309736860f7cd3d84
Rodrigo Rech <rodrigorech92@gmail.com>
2021-04-28 16:07:20 -0300

    img renderized for each movie

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 function MovieCard(props) {
   console.log(props);
-  const { title, subtitle } = props;
+  const { title, subtitle, storyline, imagePath, rating } = props;
   console.log(title);
   return (
     <div key={ title }>
       <p>{ title }</p>
       <p>{ subtitle }</p>
+      <img src={ imagePath } alt={ `${title}` } />
     </div>
   );
 }
