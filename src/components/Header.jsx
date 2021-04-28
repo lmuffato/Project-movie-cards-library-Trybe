5828dfb65fd7806ee4de88aa1a1e6a31db377747
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-04-28 15:36:42 -0300

    Adicionando correções do lint e o requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Header() {
-    <h1></h1>
+  return (
+    <header>
+      <h1>Movie Cards Library</h1>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
