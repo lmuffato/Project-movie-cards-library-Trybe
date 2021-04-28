9ef6eba470bd682c3f50ddea0aa4d3df295d8626
ThalitaC <thalitaceci@gmail.com>
2021-04-28 20:34:52 -0300

    Requisito 4: Renderize componentes <MovieCard /> dentro de <MovieList />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return 'test';
+    const { movies } = this.props;
+    return movies.map((movie) => <MovieCard key={ movie.title } />);
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
