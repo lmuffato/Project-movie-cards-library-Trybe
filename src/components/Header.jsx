dbf5b52f0ceff5c388b0906362891dfd18d6b6a4
luanlsr <luanfswd@gmail.com>
2021-04-28 17:15:46 -0300

    requisito 1 finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,13 @@
 // implement Header component here
-// Iniciando o projeto 🚀
+// Iniciando o projeto 🚀tus
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <h1>Olá</h1>
+    );
+  }
+}
+
+export default Header;
