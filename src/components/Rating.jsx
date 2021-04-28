abad67f7c648dd00f83168da030917f8cdd40364
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-04-28 16:54:36 -0300

    adicionando requisito 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,21 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { movie } = this.props;
+    const { rating } = movie;
+    return (
+      <fieldset>
+        <p>{rating}</p>
+      </fieldset>
+    );
+  }
+}
+
+Rating.propTypes = {
+  movie: propTypes.object,
+  rating: propTypes.number,
+};
+
+export default Rating;
