71bc3bcd20ed1d8c4fd60309165e08ae55c9a6cd
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-04-28 19:05:36 -0300

    finalizando projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,18 @@
 // implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { value } = this.props;
+    return (
+      <div className="rating">{value}</div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+export default Rating;
