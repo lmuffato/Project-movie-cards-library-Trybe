b07125373d3da13689a856bc65a6bcd000cbb8be
Edrei <edreimateus@gmail.com>
2021-04-28 17:42:25 -0300

    fix lint issues

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Protypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -12,10 +13,19 @@ class MovieCard extends React.Component {
           <h1>{subtitle}</h1>
           <p>{storyline}</p>
         </div>
-        <img src={ imagePath } alt={ `${title}` } />
+        <img src={ imagePath } alt={ title } />
       </div>
     );
   }
 }
 
+MovieCard.propTypes = {
+  movie: Protypes.arrayOf(Protypes.shape({
+    title: Protypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  })).isRequired,
+};
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -11,5 +12,19 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })),
+};
+
+MovieList.defaultProps = {
+  movies: [],
+};
 
 export default MovieList;
