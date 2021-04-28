e446a4b5057b18eab780fa4ee68c474ff3c255d5
andy silva <andssilva.23@gmail.com>
2021-04-28 18:04:04 -0400

    feat(requisito 12): :rocket: Renderiza nota de um filme dentro de  <Rating />

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,22 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <> </>
+      <div className="movie-card-rating">
+        <span className="rating">{ rating }</span>
+      </div>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.shape({
+    rating: PropTypes.number
+  }),
+};
 
 export default Rating;
