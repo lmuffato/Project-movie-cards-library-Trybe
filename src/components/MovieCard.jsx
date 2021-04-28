46bc5320d01a351af256144c0e7268981714ee48
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-04-28 18:06:31 -0300

    Corrigindo alguns erros

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,21 +3,21 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    const { movie } = this.props;
-    const { title, subtitle, storyLine, imagePath } = movie;
+    const { movies } = this.props;
+    const { title, subtitle, imagePath } = movies;
     return (
       <fieldset>
         <img src={ imagePath } alt="Imagem do filme" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
-        <p>{storyLine}</p>
+        {/* <p>{storyLine}</p> */}
       </fieldset>
     );
   }
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes.shape({
+  movies: PropTypes.shape({
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
