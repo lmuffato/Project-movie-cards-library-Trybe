384e1f245096816f830eda5372e3a47f92477e73
Thiago <thiagodesouza.dev@gmail.com>
2021-04-28 17:31:19 -0300

    1 - Crie um componente <Header />

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
 // implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return <img src={this.props.source} alt={this.props.alternativeText} />;
+  }
+}
+export default Header;
