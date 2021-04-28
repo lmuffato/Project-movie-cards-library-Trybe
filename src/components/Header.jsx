dacfff701395521c012d9ed6e7d16fffea2174cd
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-28 16:07:26 -0300

    Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
 // implement Header component here
+// Requisito 1
+import React from 'react';
+
+function Header() {
+//   const titulo = <header> </header>;
+  return (
+    // titulo
+    <header> </header>
+  );
+}
+
+export default Header;
