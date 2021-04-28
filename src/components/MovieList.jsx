f874bee558228c1b40e1be79a4f59e8d5a655465
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-28 20:40:01 -0300

    Movie List - Requisitos 3,4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,31 @@
 // implement MovieList component here
-// Requisito 3
+// Requisito 3, 4 e 5
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 function MovieList(props) {
+  const { movies } = props;
+
   return (
-    <main>{props}</main>
+    <main>
+      {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+    </main>
   );
 }
+
+// Requisito 17
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.number,
+  })),
+};
+
+MovieList.defaultProps = {
+  movies: [],
+};
 
 export default MovieList;
