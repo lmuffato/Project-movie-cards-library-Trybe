8d6c5c3afecd975148fb2ac4bb8654df41928703
nascjoao <oi@joaonasc.dev>
2021-04-28 13:28:28 -0300

    MovieList, MovieCard e Rating: Requisitos 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+function MovieList({ movies }) {
+  return (
+    <section>
+      {movies.map((movie) => (
+        <MovieCard key={ movie.title } movie={ movie } />
+      ))}
+    </section>
+  );
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Rating({ rating }) {
+  return <span>{rating}</span>;
+}
+
+Rating.propTypes = { rating: PropTypes.number }.isRequired;
+
+export default Rating;
