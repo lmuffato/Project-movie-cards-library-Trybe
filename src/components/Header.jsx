d2811089d115fdb43da2ad8c930c5886cb8d9edd
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-04-28 15:43:05 -0300

    :rocket: Requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
