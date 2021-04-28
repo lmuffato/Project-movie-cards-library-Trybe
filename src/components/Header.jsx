964b404f030e145bf528deeb3a1cd5576e159e95
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-04-28 15:49:55 -0300

    começa, requisitos 1,2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,10 @@
-// implement Header component here
+import React from 'react';
+
+function Header() {
+  return (
+    <section className="movie-card-header">
+      <h1>Bora começar</h1>
+    </section>
+  );
+}
+export default Header;
