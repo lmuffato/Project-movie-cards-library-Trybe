41fa0d0b4a99198ab1a4618933a5665f8f5d7fb0
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-04-28 17:36:07 -0300

    Ok

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
 // implement MovieCard component here
+
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <h1>Oi</h1>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (this.props);
+  }
+}
+
+export default MovieList;
