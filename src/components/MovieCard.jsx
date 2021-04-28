ae2d3c7319c9487232b6262b9f9402e281b0998f
Bruno Mendes <mendessbruno@gmail.com>
2021-04-28 16:34:49 -0300

    resolvido requisito 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
-// implement MovieCard component here
+import React from 'react';
+
+function MovieCard(props) {
+  const { title, subtitle, storyline, imagePath, rating } = props.movie;
+  return (
+    <section>
+      <img src={ imagePath } alt="capa do filme" />
+    </section>
+  );
+}
+
+export default MovieCard;
