8b23dd03215600e7d3942098c75e4fffff7ba51f
jonathan <jonathanfernandesjf98@gmail.com>
2021-04-28 20:42:15 -0300

    projeto quase pronto, faltando requisito 12 e validações por propsType

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <div>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <img src={imagePath} alt="magem do filme" />
+        <p>{storyline}</p>
+        <Rating rating={rating} />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies
+          .map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
