a20b9b37a8466b0f705c9b1920df89389988da3e
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-04-28 17:23:17 -0300

    requisito 3,4,5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React from 'react';
+
+function MovieCard() {
+  // const { movieInfo } = props;
+
+  return (
+    <section>
+      {/* <h3> { movieInfo.title } </h3> */}
+    </section>
+  );
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieList(props) {
+  const { movies } = props;
+  console.log(movies);
+
+  return (
+    <section>
+      { movies.map((movie) => <MovieCard key={ movie.title } movieInfo={ movie } />) }
+    </section>
+  );
+}
+
+MovieCard.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
+
+export default MovieList;
