1e3dfe6d58089cbdc1129ad47327e0ad8f3f380f
defauth98 <neto.daniribeiro@gmail.com>
2021-04-28 15:57:54 -0300

    Requisito 06

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
-function MovieCard(){
+function MovieCard(props){
+    const {title, subtitle, storyline, imagePath, rating} = props
+
     return (
-        <h1>Movie Card</h1>
+        <div>
+            <h1>{title}</h1>
+            <h2>{subtitle}</h2>
+            <p>{storyline}</p>
+            <img src={imagePath} alt={title}/>
+            <span>{rating}</span>
+        </div>
     )
 }
 
