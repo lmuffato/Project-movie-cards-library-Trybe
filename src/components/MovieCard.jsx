ede752c6394bfb8f5039c8ecd92a619d3988bb23
Bruno Mendes <mendessbruno@gmail.com>
2021-04-28 17:24:58 -0300

    resolvidos requisitos ate o 15

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import Rating from './Rating';
 
-function MovieCard(props) {
-  const { title, subtitle, storyline, imagePath, rating } = props.movie;
-  return (
-    <section>
-      <img src={ imagePath } alt="capa do filme" />
-      <h4>{ title }</h4>
-      <h5>{ subtitle }</h5>
-      <p>{ storyline }</p>
-    </section>
-  );
-}
+const MovieCard = class extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <section>
+        <img src={ imagePath } alt="capa do filme" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </section>
+    );
+  }
+};
 
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,11 @@
-// implement Rating component here
+import React from 'react';
+
+function Rating(props) {
+  return (
+    <section className="rating">
+      { props.rating }
+    </section>
+  );
+}
+
+export default Rating;
