c5ddae51756380d0b0c470b24e61261f6ff6474f
Alexandre Oliveira <alexandremolive@gmail.com>
2021-04-28 16:34:40 -0300

    terceiro requisito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
 // implement MovieCard component here
+import React from 'react';
+
+function MovieCard(props) {
+  const { movies } = props;
+  return (
+    <div className="movie-card">
+      <img className="image" src={ movies.imagePath } alt="Imagem da capa do Filme " />
+      <p className="title">{movies.title}</p>
+      <p className="subtitle">{movies.subtitle}</p>
+      <p className="story-line">{movies.storyline}</p>
+    </div>
+  );
+}
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieList(props) {
+  const { movies } = props;
+  console.log(movies);
+  return (
+    <div>
+      { movies.map((movie) => <MovieCard key={ Math.random() * 100 } movies={ movie } />)}
+    </div>
+  );
+}
+export default MovieList;
