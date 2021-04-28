0b38472ed9a55dc258e73f08d921af0b7fe55cbd
Thiago <thiagodesouza.dev@gmail.com>
2021-04-28 18:12:52 -0300

    3 - Crie um componente <MovieList />

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <Header />
+    <MovieList />
   );
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react'
+
+class MovieList extends React.Component {
+render () {
+  return (
+    <h1>adasda</h1>
+  );
+}
+}
+
+export default MovieList;
