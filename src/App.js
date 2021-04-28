db8a4523774ef6c43ee374dc79e4ba28e3caa926
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-04-28 16:04:03 -0300

    :rocket: Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList
+        movies={
+          [{ name: 'Piratas do Caribe' }, { name: 'O Senhor dos Anéis' }]
+        }
+      />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <ul>
+        {movies.map((movie, index) => <li key={ index }>{movie.name}</li>)}
+      </ul>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
