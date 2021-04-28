653441bc24b1c3c4f1a69dd879d93b32ec3b6921
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-04-28 19:24:51 -0300

    :performing_arts: Melhorando o layout do MovieList

--- src/App.js
+++ src/App.js
@@ -27,6 +27,72 @@ function App() {
       imagePath: 'https://images.unsplash.com/photo-1506466010722-395aa2bef877?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1488&q=80',
       rating: 10,
     },
+    {
+      title: 'Piratas do Caribe',
+      subtitle: 'O Prisioneiro de Azcabam',
+      storyline: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                  Maxime accusantium ab earum! 
+                  Natus, quibusdam, voluptates quaerat molestias 
+                  velit cupiditate nemo neque 
+                  placeat aspernatur exercitationem`,
+      imagePath: 'https://images.unsplash.com/photo-1611125967908-0d14a7889ff5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+      rating: 10,
+    },
+    {
+      title: 'O Senhor dos Anéis',
+      subtitle: 'As Duas Torres',
+      storyline: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                  Maxime accusantium ab earum! 
+                  Natus, quibusdam, voluptates quaerat molestias 
+                  velit cupiditate nemo neque 
+                  placeat aspernatur exercitationem`,
+      imagePath: 'https://images.unsplash.com/photo-1506466010722-395aa2bef877?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1488&q=80',
+      rating: 10,
+    },
+    {
+      title: 'Piratas do Caribe',
+      subtitle: 'O Prisioneiro de Azcabam',
+      storyline: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                  Maxime accusantium ab earum! 
+                  Natus, quibusdam, voluptates quaerat molestias 
+                  velit cupiditate nemo neque 
+                  placeat aspernatur exercitationem`,
+      imagePath: 'https://images.unsplash.com/photo-1611125967908-0d14a7889ff5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+      rating: 10,
+    },
+    {
+      title: 'O Senhor dos Anéis',
+      subtitle: 'As Duas Torres',
+      storyline: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                  Maxime accusantium ab earum! 
+                  Natus, quibusdam, voluptates quaerat molestias 
+                  velit cupiditate nemo neque 
+                  placeat aspernatur exercitationem`,
+      imagePath: 'https://images.unsplash.com/photo-1506466010722-395aa2bef877?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1488&q=80',
+      rating: 10,
+    },
+    {
+      title: 'Piratas do Caribe',
+      subtitle: 'O Prisioneiro de Azcabam',
+      storyline: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                  Maxime accusantium ab earum! 
+                  Natus, quibusdam, voluptates quaerat molestias 
+                  velit cupiditate nemo neque 
+                  placeat aspernatur exercitationem`,
+      imagePath: 'https://images.unsplash.com/photo-1611125967908-0d14a7889ff5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+      rating: 10,
+    },
+    {
+      title: 'O Senhor dos Anéis',
+      subtitle: 'As Duas Torres',
+      storyline: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                  Maxime accusantium ab earum! 
+                  Natus, quibusdam, voluptates quaerat molestias 
+                  velit cupiditate nemo neque 
+                  placeat aspernatur exercitationem`,
+      imagePath: 'https://images.unsplash.com/photo-1506466010722-395aa2bef877?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1488&q=80',
+      rating: 10,
+    },
   ];
 
   return (
