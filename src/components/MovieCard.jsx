5db30925f1291b6a35e0477f066c5c2403a86782
LucasMLara <lucasml@live.com>
2021-04-28 17:34:01 -0300

    MovieCard Implement - req 8 and 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-function MovieCard({ movie: { title, subtitle, storyline, imagePath, rating } }) {
-return <img src={ imagePath } alt="FilmCardImg" />;
+function MovieCard({
+  movie: { title, subtitle, storyline, imagePath, rating },
+}) {
+  return (
+    <>
+      <img src={ imagePath } alt="FilmCardImg" />
+      <h4>{title}</h4>
+      <h5>{subtitle}</h5>
+    </>
+  );
 }
 export default MovieCard;
+
+// https://pt-br.reactjs.org/docs/fragments.html
