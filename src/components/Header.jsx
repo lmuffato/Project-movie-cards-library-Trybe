089288dde91585ce71976faed23e2f231623a663
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-04-28 18:50:05 -0300

    Corrige ESlint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-function Header () {
-    return <h1>Movie Cards Library</h1>
+function Header() {
+  return <h1>Movie Cards Library</h1>;
 }
 
 export default Header;
-
