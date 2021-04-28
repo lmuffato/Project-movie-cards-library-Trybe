91a179ae2030bdf6b621e65f7966776830217459
Lucas Muffato <lmiremuffato@gmail.com>
2021-04-28 18:12:38 -0300

    requisito 01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 // import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header.jsx';
+import Header from './components/Header.JSX';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-        </div>
+    <div className="App"><Header /></div>
   );
 }
 
