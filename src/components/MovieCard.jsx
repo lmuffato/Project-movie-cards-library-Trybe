9537360beb558ab50e9ff534de2e45c452e9f44a
andy silva <andssilva.23@gmail.com>
2021-04-28 17:10:10 -0400

    feat(requisito 10): :rocket: Renderiza sinopse dentro de uma tag p

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,8 +10,11 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt="movie" />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
       </div>
     );
   }
