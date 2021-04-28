6280bacb2de166c6acef0cbc24120f77051df97f
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-04-28 16:08:27 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
+/* eslint-disable import/no-unresolved */
 // implement MovieList component here
+import React from 'react';
+
+function MovieList() {
+  return (
+    <section className="movie-list">
+      <span>Movie List</span>
+    </section>
+  );
+}
+
+export default MovieList;
