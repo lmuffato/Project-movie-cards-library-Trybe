88e6bb67d6e39a83826781ba7e677f244bc88b09
Samuel Melo <samuelmmjunior@gmail.com>
2021-04-28 15:33:40 -0300

    requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// implement Header component here
+import React from 'react'
+
+function Header() {
+    return (
+      <header>
+          Titulo
+      </header>
+    );
+  }
+  
+  export default Header;
