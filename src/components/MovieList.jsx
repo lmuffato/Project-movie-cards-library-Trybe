07b1073c131176107269a26a3731d0a57d7efb84
Bruno Mendes <mendessbruno@gmail.com>
2021-04-28 16:26:06 -0300

    resolvido requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function Movielist(props) {
+  return (
+    <section>
+      {props.movies.map((movie) => <MovieCard movie = { movie } />)}
+    </section>
+  );
+}
+
+export default Movielist;
