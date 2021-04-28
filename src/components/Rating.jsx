8f7eef44bffb6f91d4184d3444422c29447d63c2
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-04-28 13:31:33 -0300

    requisito 11 feito

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 
-Class Rating extends React.Component {
-    
+class Rating extends React.Component {
+  render () {
+    const { rating } = this.props;
+    return (
+        <div>
+        { rating }
+        </div>
+    )
+  }
 }
+
+Rating.Proptypes = {
+    Rating: Proptypes.string.isRequired,
+}
+
+export default Rating;
