f6360ac52d0a06f09927e1cab4b842935f1e540d
Ana Ventura <anaventura1811@gmail.com>
2021-04-28 15:35:23 -0300

    Requisito 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function MovieCard(props) {
+  const { title, subtitle } = props;
+
+  return (
+    <>
+      <h4>{ title }</h4>
+      <h5>{ subtitle }</h5>
+    </>
+  );
+}
+
+MovieCard.defaultProps = {
+  title: 'Título do filme',
+  subtitle: 'Subtítulo do filme',
+};
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+};
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+export default function MovieList({ movies }) {
+  return (
+    <>
+      {movies.map((movie) => (<MovieCard key={ movie.rating } />))}
+    </>
+  );
+}
+
+MovieList.defaultProps = {
+  movies: {},
+};
+
+MovieList.propTypes = {
+  movies: {
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+  },
+};
