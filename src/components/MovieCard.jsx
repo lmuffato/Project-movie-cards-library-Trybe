ca26667212f81a5dc2773e08682dc5f77b135d9b
guidugaich <guidugaich.dev@gmail.com>
2021-04-28 18:08:45 -0300

    created MovieCard component and added info from movies data

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types'
+
+function MovieCard(props) {
+  const { title, subtitle, storyline, rating, imagePath } = props.movie;
+  return (
+    <div>
+      <img src={imagePath} />
+      <h4>{title}</h4>
+      <h5>{subtitle}</h5>
+      <p>{storyline}</p>
+    </div>
+
+  );
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieList(props) {
+  return (
+    props.movies.map((mov) => (
+      <MovieCard key={ mov.title } movie={ mov } />
+    ))
+  );
+}
+
+export default MovieList;
