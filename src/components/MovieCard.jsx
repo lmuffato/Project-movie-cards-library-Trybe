fb9eb8bbdb20ef0e56beb06ce39b5514656129c1
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-04-28 16:00:38 -0300

    requisito 12 corrigido

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,7 +13,7 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <Rating className="rating" rating={ rating } />
+          <Rating className="movie-card-rating" rating={ rating } />
         </div>
       </div>
     );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,7 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className="movie-card-rating">
+      <div className="rating">
         { rating }
       </div>
     );
