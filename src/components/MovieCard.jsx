1a9865be15f2742a533d5e57406d1d5daec6031c
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-04-28 16:01:30 -0300

    Adicionando requisito 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyLine, imagePath } = movie;
+    return (
+      <fieldset>
+        <img src={ imagePath } alt="Imagem do filme" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyLine}</p>
+      </fieldset>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        {movies.map((movie) => (
+          <MovieCard movie={ movie } key={ movie.title } />
+        ))}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
