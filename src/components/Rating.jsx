4fcf0174bde0632fd2e1f482766dbee4bb5606bc
Willian Mendes <willianmendesbr@gmail.com>
2021-04-28 17:05:12 -0300

    requisito 13 e 14

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
-function Rating() {
-  return (
-    <div />
-  );
+function Rating(props) {
+  return props.rating;
 }
+
+Rating.propsTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
