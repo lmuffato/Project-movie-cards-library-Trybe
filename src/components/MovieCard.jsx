1f61367ba73ed0171a5aadeb1beb7e594115f158
Marcus C. H. Filho <mchusemann@gmail.com>
2021-04-28 17:54:18 -0300

    :rocket: feat: Todos os requisitos concluidos.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,32 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+function MovieCard({ movie }) {
+  const { title, subtitle, storyline, imagePath, rating } = movie;
+  return (
+    <div>
+      <h4>{title}</h4>
+      <h5>{subtitle}</h5>
+      <p>{storyline}</p>
+      <Rating rating={ rating } />
+      <img src={ imagePath } alt="Imagem Do Filme" />
+    </div>
+  );
+}
+
+PropTypes.shape({ root: PropTypes.string.isRequired });
+
+// Proptypes Source: https://www.youtube.com/watch?v=ldhZ2L3XYis
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieList({ movies }) {
+  return (
+    movies.map((movie) => {
+      const { title } = movie;
+      return <MovieCard movie={ movie } key={ title } />;
+    })
+  );
+}
+
+export default MovieList;
