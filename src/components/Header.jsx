bb89d2e86adf2ef129abf9595ac083a73f4df930
Hernanne <hernannedomiciano@gmail.com>
2021-04-28 15:21:52 -0300

    Requisitos 1 e 2 feitos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component herea
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
