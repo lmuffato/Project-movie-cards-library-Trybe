36bee7c1c15fd0b353983ce9b24d5eb48ebb7f7c
Lucas Brito <lucasbrito.carneiro@gmail.com>
2021-04-29 11:11:07 -0300

    Fix Lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import MovieCard from './MovieCard'
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-        <section>
-            {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-        </section>
+        <div className="movie-list">
+          {movies.map((movie) => (<MovieCard key={ movie.title } movie={ movie } />))}
+        </div>
     )
   }
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 class Rating extends React.Component {
-    render(){
-        const { rating } = this.props
+    render() {
+        const rating = this.props.rating
         return(
-            <div>{ rating }</div>
+            <div className='rating'>{ rating }</div>
         )
     }
 }
