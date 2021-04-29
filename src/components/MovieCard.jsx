a1f166baff3b5e882af76d6975f4c1c63215e979
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-28 23:51:02 -0300

    Adiciona proptypes em MovieCard e MovieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -15,5 +16,15 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  })).isRequired,
+};
 
 export default MovieCard;
