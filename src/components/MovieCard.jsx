5a5f3304daccc3ddeed4cb7875a1f6f36fe26e1d
johnpligeiro <johnpligeiro@gmail.com>
2021-04-29 08:20:15 -0300

    Teste 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      'Hello World'
+    );
+  }
+}
+
+export default MovieCard;
