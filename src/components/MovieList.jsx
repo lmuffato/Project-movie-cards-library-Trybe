f9587a4ef2a328ec66bcfd45430ccc82d8432274
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-04-28 21:42:53 -0300

    Adiciona componente

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import MovieCard from './components/MovieCard'
+import MovieCard from './MovieCard';
 
 function MovieList() {
   return (
     <section>
-        <MovieCard />
+      <MovieCard />
     </section>
   );
 }
