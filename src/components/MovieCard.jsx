0e60241008a7db00a4a407927a3cd1ed48496e93
LucasMLara <lucasml@live.com>
2021-04-28 21:42:20 -0300

    FIMMMM

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 function MovieCard({
   movie: { title, subtitle, storyline, imagePath, rating },
@@ -9,9 +11,19 @@ function MovieCard({
       <h4>{title}</h4>
       <h5>{subtitle}</h5>
       <p>{storyline}</p>
+      <Rating rating={ rating } />
     </>
   );
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({ title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired }).isRequired,
+};
+
 export default MovieCard;
 
 // https://pt-br.reactjs.org/docs/fragments.html
