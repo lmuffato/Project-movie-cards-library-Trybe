47d422a1cdc30bc7b29ae93af45ef06a1b1aebd6
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-04-28 21:17:11 -0300

    Adiciona movieCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+// import DataMovies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './components/MovieCard'
+
+function MovieList() {
+  return (
+    <section>
+        <MovieCard />
+    </section>
+  );
+}
+
+export default MovieList;
