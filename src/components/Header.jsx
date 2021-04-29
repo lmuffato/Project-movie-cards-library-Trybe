d32b2a9b00c56770b61e9342d758f4977695c0bb
carlos25xrm <carlos25xrm@gmail.com>
2021-04-29 13:47:36 -0300

    Refatorando o arquivo Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1>Movie Cards Library</h1>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header>
+      <h1>Movie Cards Library</h1>
+    </header>
+  );
 }
 
 export default Header;
