0e6b71d38725530d8ebff42c455b3befc39887a4
Thiago <thiagodesouza.dev@gmail.com>
2021-04-28 21:37:51 -0300

    11 - Crie um componente <Rating />

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
 // implement Rating component here
+import React from 'react';
+
+class Hating extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+export default Hating;
