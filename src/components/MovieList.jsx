a348f6ea6ca8cab24ec954d94ddae5bfe93e3155
André Jaques Batista <andrejaques.b@gmail.com>
2021-04-29 08:14:39 -0300

    add require 03

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+/* import PropTypes from 'prop-types'; */
+/* import MovieCard from './movieCard'; */
+
+class MovieList extends React.Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <div className="movie-list">
+        La vai movies
+      </div>
+    )
+  }
+}
+
+export default MovieList
