d7be2d77622e299e3ec46fe4238c2a13b09afc28
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-28 23:53:13 -0300

    Atualiza lint do ultimo commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline} = movie;
+    const { imagePath, title, subtitle, storyline } = movie;
 
     return (
       <div>
