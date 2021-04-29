08983f0c8841b95fb564e807a910549a28447f3f
johnpligeiro <johnpligeiro@gmail.com>
2021-04-29 08:53:42 -0300

    MovieList e Rating

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+
     return (
-      <MovieCard />
+      <div>
+        { movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />) }
+      </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })),
+};
+
+MovieList.defaultProps = {
+  movies: [],
+};
 
 export default MovieList;
