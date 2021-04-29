21e7115ed937e312d2515b9d6f669df64862f220
walvesb65 <willian.alvesb65@gmail.com>
2021-04-28 22:30:56 -0300

    É bombaaaaaa, isso aqui ta uma loucura

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((Play) => <MovieCard key={ Play.title } movies={ Play } />) }
+      </div>
+    );
+  }
+}
+
+export default MovieList;
