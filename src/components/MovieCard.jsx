b77fdd41d8d534848705b4a572c07e6219f56b09
Thiago <thiagodesouza.dev@gmail.com>
2021-04-29 19:43:48 -0300

    12 - Renderize a nota de um filme dentro de Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 // implement MovieCard component here
-import React from 'react';
-import PropTypes from 'prop-types';
-import Rating from './Rating';
+import React from "react";
+import PropTypes from "prop-types";
+import Rating from "./Rating";
 
 class MovieCard extends React.Component {
   render() {
@@ -9,11 +9,13 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <div className="movie-card">
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <img src={ imagePath } alt="{title}" />
-        <Rating rating={ rating } />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <img className="movie-card-image" src={imagePath} alt="{title}" />
+        </div>
+        <Rating className="movie-card-rating" rating={rating} />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -7,7 +7,7 @@ class Rating extends React.Component {
     const { rating } = this.props;
     return (
       <div className="movie-card-rating">
-        <span>{rating}</span>
+        <span className="rating">{rating}</span>
       </div>
     );
   }
