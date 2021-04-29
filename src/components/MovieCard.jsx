48f6fbb355cde609b862f957bfea82f631794fdf
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-28 23:30:23 -0300

    Requisitos 5 , 6, 7 , 8, 9, 10 com correçoes no lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline, rating } = movie;
+    const { imagePath, title, subtitle, storyline} = movie;
 
     return (
       <div>
-        <img src={ imagePath }></img>
+        <img src={ imagePath } alt="imagem" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
