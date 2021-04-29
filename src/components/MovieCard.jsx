2fe7d0c2a72b3d85b9edb53c5dc20b1ed0ef9f27
johnpligeiro <johnpligeiro@gmail.com>
2021-04-29 08:29:36 -0300

    Renderizando elementos de MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline } = movie;
+
     return (
-      'Hello World'
+      <div>
+        <img src={ imagePath } alt="poster" />
+        <div>
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+        </div>
+      </div>
     );
   }
 }
