0363424bfc863bb398dd4aae769fefb8825201f0
Stanrley Barbosa <3011stan@gmail.com>
2021-04-28 21:56:01 -0300

    Requisitos de 1 ao 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
 // implement MovieCard component here
+import React from 'react';
+// import Image from './Image';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    // console.log(this.props.movieObj.title);
+    const { movie } = this.props;
+    return (
+      <div className='movie-card'>
+        <img src={movie.imagePath} alt={movie.title}></img>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyLine: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }),
+};
+
+export default MovieCard;
+
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    // console.log(this.props.movies);
+    return (
+      this.props.movies.map((movie) => <MovieCard key={movie.title} movie={movie} />
+      )
+    );
+  }
+}
+
+export default MovieList;
