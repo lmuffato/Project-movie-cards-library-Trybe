6f126f5f103d92226e82e6c1b398bd046a1eab8b
lucas-chave <lcchavesr150@gmail.com>
2021-04-28 22:11:22 -0300

    requisito 6, 7, 8, 9 e e10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
-// implement MovieCard component here
+// import { prototype } from 'enzyme-adapter-react-16';
+import React from 'react';
+import '../css/movieCard.css';
+
+function MovieCard(prop) {
+  const { movie } = prop;
+  // console.log(movies);
+
+  return (
+    <div className="cart-film">
+      <img className="image-film" src={ movie.imagePath } alt={ movie.title } />
+      <h4 className="title-film">{ movie.title }</h4>
+      <h5 className="subtitle-film">{ movie.subtitle }</h5>
+      <p className="storyline-film">{ movie.storyline }</p>
+      {/* <p className="rating-film">{ film.rating }</p> */}
+    </div>
+  );
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import MovieCard from './MovieCard';
+import '../css/movieList.css';
 
-function MovieList() {
-  return (<section>MovieList</section>);
+function MovieList(props) {
+  const movies = props;
+  console.log(movies);
+  return (
+    <div className="container">
+      {movies.movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+    </div>);
 }
 
 export default MovieList;
