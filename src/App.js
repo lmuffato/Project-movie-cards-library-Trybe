cb74c19bacae80f7c623733a9a70f0d84bc9e3dd
Fernando Resende <fernandocastellob@gmail.com>
2021-04-30 17:44:20 -0300

    requisito 03 e 04 feito

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import Titulo from './components/Header';
+import MovieCard from './components/MovieCard';
+import { movies } from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <Titulo />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Titulo />
+        <MovieCard movie={ movies } />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <section>
+        <div>
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+          <span>{rating}</span>
+          <img src={ imagePath } alt="Imagem do filme" />
+        </div>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
