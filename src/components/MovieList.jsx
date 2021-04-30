19c2120668051a7809d2d5a50284f6bd6cacd54f
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-30 10:46:42 -0300

    Fourth requirement done

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
 
     return (
-      <ul>{movies}</ul>
+      <ul>
+        {movies.map((movie, index) => <MovieCard key={ index } movie={ movie } />)}
+      </ul>
     );
   }
 }
