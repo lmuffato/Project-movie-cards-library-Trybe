e90b8303f6d21ead15a2badf0c65da99b47e58e4
Mateus Alencar <mtauws@gmail.com>
2021-04-30 18:05:48 -0300

    some comments

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,9 @@ import MovieCard from './MovieCard';
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
+
     return (
+      // Source: https://github.com/tryber/sd-09-project-movie-cards-library/tree/45bd19080254fcabbc0964060bf93c3334899c9d
       <section className="movie-list">
         { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </section>
@@ -13,6 +15,7 @@ class MovieList extends Component {
   }
 }
 
+// Source: https://github.com/tryber/sd-09-project-movie-cards-library/tree/45bd19080254fcabbc0964060bf93c3334899c9d
 MovieList.propTypes = {
   movies: PropTypes.objectOf(PropTypes.string).isRequired,
 };
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
+    // Source: https://github.com/tryber/sd-09-project-movie-cards-library/tree/45bd19080254fcabbc0964060bf93c3334899c9d
     const { rating } = this.props;
     return (
       <div className="rating">
