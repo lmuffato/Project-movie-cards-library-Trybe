e77e2cde3f69ca4daf08780483486dc3299b0231
Gustavo Sousa <gustavohl98@gmail.com>
2021-04-30 19:10:56 -0300

    Requisito 07 - Renderiza imagem do filme

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+
     return (
-      <p>This is a movie</p>
+      <div>
+        <img src={ movie.imagePath } alt="Movie" />
+        <p>This is a movie</p>
+      </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,9 +5,9 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    console.log(movies);
 
-    const movieCards = movies.map((movie) => <MovieCard key={ movie.title } />);
+    const movieCards = movies
+      .map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
     return (
       <div>
         { movieCards }
