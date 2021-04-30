3ac04157fe612aab0d86b01b5e60dce44c8a0728
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-04-29 21:59:32 -0300

    testando requisitos

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Componet {
+  render() {
+      const { rating } = this.props;
+    return (
+      <div className='rating'>{rating}</div>
+    );    
+  };
+};
+
+export default Rating;
