e8d4ae38931555f6acc285980147cf7517b837fc
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-04-29 22:38:39 -0300

    correcao do lint4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline, rating } = movie;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <section className="movie-card">
-        <img className="movie-card-image" src={ imagePath } alt={ title }/>
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
