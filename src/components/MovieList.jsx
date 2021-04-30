a8ec9d290c301db642bf606af770550652703d83
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-30 10:33:23 -0300

    Third requirement done

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <ul>{movies}</ul>
+    );
+  }
+}
+
+export default MovieList;
