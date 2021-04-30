58d9db56e308e58deb8599d433dcff75a0712384
Priscila <prihscapin@gmail.com>
2021-04-30 19:15:10 -0300

    Requisitos 1 e 2 - Linter issues

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+// import React from 'react';
+// import './MovieList.css';
+
+// // Crie um componente que represente toda a área com os cartões de filmes. <MovieList />
+// // deve receber uma prop movies, que é um array de objetos com informações de um filme
+// class MovieList extends React.Component {
+//   render() {
+// //     return (<div>
+// //       {this.props.movies}
+// //     </div>);
+// //   }
+// }
+// export default MovieList;
