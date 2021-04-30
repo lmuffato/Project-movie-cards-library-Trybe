5b36eedacfe051c0b1a9b53bd279b5214165043e
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-04-29 22:53:21 -0300

    movieList line6

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
