7f7639638abae902086aa9c39e4e903f4c38c58e
Priscila <prihscapin@gmail.com>
2021-04-30 20:06:12 -0300

    Requisito 3 <3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 class App extends React.Component {
   render() {
     // const text = 'Teste de renderização';
     // ReactDOM.render(text, document.getElementsByClassName('Header'));
     return (
-      <Header />
+      <div>
+        <Header />
+        <MovieList />
+      </div>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,14 @@
-// import React from 'react';
+import React from 'react';
 // import './MovieList.css';
 
-// // Crie um componente que represente toda a área com os cartões de filmes. <MovieList />
-// // deve receber uma prop movies, que é um array de objetos com informações de um filme
-// class MovieList extends React.Component {
-//   render() {
-// //     return (<div>
-// //       {this.props.movies}
-// //     </div>);
-// //   }
-// }
-// export default MovieList;
+// Crie um componente que represente toda a área com os cartões de filmes. <MovieList />
+// deve receber uma prop movies, que é um array de objetos com informações de um filme
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div> Movie List</div>
+    );
+  }
+}
+export default MovieList;
