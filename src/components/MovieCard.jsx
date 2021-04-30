a9540b5ca70f6c0c0176c1fdf6d52a8465fde894
Debora Passos <deborinhapassos@gmail.com>
2021-04-30 10:44:14 -0300

    Commitao bomba

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    return 'teste';
+    const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
+    return (
+      <section>
+        <img src={ imagePath } alt="Movie" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={ rating } />
+      </section>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  imagePath: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 export default MovieCard;
