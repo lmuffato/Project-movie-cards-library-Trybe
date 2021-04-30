5b6bd2e0ef32ec128100126252609374bace8431
Priscila <prihscapin@gmail.com>
2021-04-30 18:55:42 -0300

    Requisitos 1 e 2 <3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
-// First commit <3
+// import { render } from 'enzyme';
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+export default Header;
