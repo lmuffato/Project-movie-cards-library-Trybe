d48fcb1afe72e898c112d2adf7383a24ca79bcaa
William <w.willers@outlook.com>
2021-05-01 12:12:04 -0400

    Requisitos 1 ao 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    // const { movies } = this.props;
+    return (
+      <ul className="movie-list">
+        <li>Teste</li>
+      </ul>
+    );
+  }
+}
+
+export default MovieList;
