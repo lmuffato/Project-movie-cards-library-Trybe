ea47593f0ac37289320b28926cc895d00eb87104
William <w.willers@outlook.com>
2021-05-01 14:32:57 -0400

    Rating component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
         <img src={ imagePath } alt={ title } className="movie-card-image" />
         <div className="movie-card-body">
-          <h3 className="movie-card-title">{ title }</h3>
-          <h4 className="movie-card-subtitle">{ subtitle }</h4>
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
+          <Rating rating={ rating } />
         </div>
       </div>
     );
@@ -24,6 +26,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
