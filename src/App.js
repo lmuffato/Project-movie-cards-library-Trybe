6e53270567bee4e7e0044d34e27bbf0ca50509cb
Lucas Muffato <lmiremuffato@gmail.com>
2021-05-02 18:54:54 -0300

    problema no lint

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-// import MovieList from './components/MovieList';
-// import data from './data';
+import MovieList from './components/MovieList';
+import data from './data';
 
 function App() {
   return (
-    <div className="App"><Header /></div>
+    <div className="App">
+      <Header />
+      <MovieList movies={ data } />
+    </div>
   );
 }
 
