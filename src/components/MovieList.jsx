1d61915c831fafe3ce7bf7a1fe934e0e2ef3b380
Maria Baeta <m.martinsdg@gmail.com>
2021-05-02 11:56:01 -0300

    Correçao lint e style

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,14 +4,16 @@ import React from 'react';
 import MovieCard from './MovieCard';
 // import movies from '../data';
 
-const MovieList = (props) => {
-  console.log(props.movies);
-  const movies = props.movies;
-  return (
-    <main className="movie-list">
-      {movies
-        .map((movie) => <MovieCard key={ movie.title } Movie={ movie } />)}
-    </main>
-  );
-};
+class MovieList extends React.Component {
+  render() {
+    const { movies } = props;
+    return (
+      <main className="movie-list">
+        {movies
+          .map((movie) => <MovieCard key={ movie.title } Movie={ movie } />)}
+      </main>
+    );
+  }
+}
+
 export default MovieList;
