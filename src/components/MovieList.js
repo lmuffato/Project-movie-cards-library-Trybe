c652036af575540fc8190169c223287148980b73
Lucas Muffato <lmiremuffato@gmail.com>
2021-05-02 18:59:04 -0300

    problema no lint

---
+++ src/components/MovieList.js
@@ -0,0 +1,14 @@
+// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    // const { movies } = this.props;
+    console.log(movies);
+    return (
+      <main className="movie-list" />
+    );
+  }
+}
+
+export default MovieList;
