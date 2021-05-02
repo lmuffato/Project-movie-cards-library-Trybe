6b450a8984ca213eabd3f33e55c6cc7f89938949
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-05-02 15:02:37 -0300

    carlos-campo-movie-cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import MovieCard from './components/MovieCard';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />
+      <div>
+        <MovieCard />
+        <MovieList />
+      </div>
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react'
+
+class MovieCard extends React.Component {
+
+  render() {
+
+
+    return (
+      <div> 
+        <h1></h1>
+      </div>
+    )
+  }
+}
+
+
+export default MovieCard;
+
