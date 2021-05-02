fd6c1ebfa22a54280ae11b706e636bb1718bc80b
Fernando Resende <fernandocastellob@gmail.com>
2021-05-02 13:19:42 -0300

    movielist finalizado

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <section>
-        {movies.map((movie) => <MovieCard movie={ movie } />)}
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </section>
     );
   }
