08965ca0366eb262ace4edee14c1a11d45cf0b40
mallmann02 <leomallmann020302@gmail.com>
2021-05-03 19:59:05 -0300

    Requirements - (11,12)

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,14 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const rating = this.props;
+    return (
+      <div className="rating">
+        { rating }
+      </div>
+    );
+  }
+}
+
+export default Rating;
