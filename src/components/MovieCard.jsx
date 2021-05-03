bef3b2c8a05faf7c7145f918d89d65eae7c684cf
johnpligeiro <johnpligeiro@gmail.com>
2021-05-03 03:09:36 -0300

    Css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,9 +8,9 @@ class MovieCard extends React.Component {
     const { imagePath, title, subtitle, storyline, rating } = movie;
 
     return (
-      <div className="movie-card-body">
+      <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt="poster" />
-        <div className="movie-card">
+        <div className="movie-card-body">
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,7 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className="rating">{ rating }</div>
+      <span className="rating">{ rating }</span>
     );
   }
 }
