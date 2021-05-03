cf65143276e6faa5b2b08038f6bae80e66a5916c
johnpligeiro <johnpligeiro@gmail.com>
2021-05-03 02:59:05 -0300

    Add css classes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,14 +8,14 @@ class MovieCard extends React.Component {
     const { imagePath, title, subtitle, storyline, rating } = movie;
 
     return (
-      <div>
-        <img src={ imagePath } alt="poster" />
-        <div>
-          <h4>{title}</h4>
-          <h5>{subtitle}</h5>
-          <p>{storyline}</p>
+      <div className="movie-card-body">
+        <img className="movie-card-image" src={ imagePath } alt="poster" />
+        <div className="movie-card">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <div>
+        <div className="movie-card-rating">
           <Rating rating={ rating } />
         </div>
       </div>
