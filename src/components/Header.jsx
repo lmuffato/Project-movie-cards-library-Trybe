8b44a9c4a0fcee50ba917563a7650314c51a8ca8
Roberto Natividade <robertonnv@gmail.com>
2021-05-02 22:29:28 -0300

    Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Header() {
- return(
-   <header>
-    <h1>Movie Cards Library</h1>
-   </header>
-        );
+  return (
+    <header>
+     <h1>Movie Cards Library</h1>
+    </header>
+  );
 }
 
 export default Header;
