0110a50f9235af6b6594f98bafe4f32473a27e44
Priscila <prihscapin@gmail.com>
2021-05-03 15:54:47 -0300

    Requisito 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return <div>this.props.movies</div>;
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import './MovieList.css';
+import MovieCard from './MovieCard';
 
 // Crie um componente que represente toda a área com os cartões de filmes. <MovieList />
 // deve receber uma prop movies, que é um array de objetos com informações de um filme
@@ -7,7 +8,9 @@ import React from 'react';
 class MovieList extends React.Component {
   render() {
     return (
-      <div> Movie List</div>
+      <div>
+        <MovieCard />
+      </div>
     );
   }
 }
