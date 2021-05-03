a21dd4027d34ca6f5719ebe41ae104a062576e1e
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-05-03 13:42:35 -0300

    Requisito 12: Renderizaçao da nota do filme

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,19 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return 'teste';
+    const { rating } = this.props;
+
+    return (
+      <p className="rating">{ rating }</p>
+    );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
