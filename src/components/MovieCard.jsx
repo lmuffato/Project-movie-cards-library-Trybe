6db4ffc326cb8f77a12547229a5663cb52381352
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-05-02 21:11:41 -0300

    Requisito 7: Feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,19 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    return 'teste';
+    const { movie } = this.props;
+
+    return (
+      <img src={ movie.imagePath } alt={ movie.imagePath } />
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf.isRequired,
+};
 
 export default MovieCard;
