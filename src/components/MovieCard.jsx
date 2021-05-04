4df542228b18e4402e1370ee489ab46e8bfe100b
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-05-04 00:52:45 -0300

    Requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 class MovieCard extends Component {
   render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div>oi</div>
+      <div>
+        <img src={ imagePath } alt="" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+      </div>
     );
   }
 }
