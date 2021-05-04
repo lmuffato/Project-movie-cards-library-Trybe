727e4c7ee6a3220a4910e0d6d6eef31d42298c6e
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-04 17:38:04 -0300

    Requisito1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,12 @@
 // implement Header component here
 // Iniciando o projeto 🚀
+
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return <h1>Movie Cards Library</h1>;
+  }
+}
+
+export default Header;
