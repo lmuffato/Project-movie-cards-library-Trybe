fc3d0b477675789f02d3053c33df1efa8fcd47c0
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-04 20:19:52 -0300

    Adiciona movies como props de MovieList

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movie="lista-de-filmes" />
+      <MovieList movie={ movies } />
     </div>
   );
 }
