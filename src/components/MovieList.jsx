6e9cf12b6068d37bf357df4d983a303b8f020284
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-05-03 23:01:45 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,2 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>{console.log(movies)}</div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
