a21d38457add308d8d36e95032f4ed5f369b70ed
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-05-04 00:29:12 -0300

    Requisito 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>{console.log(movies)}</div>
+      <div>
+        {movies.map((movie) => (<MovieCard movie={ movie } key={ movie.title } />))}
+      </div>
     );
   }
 }
