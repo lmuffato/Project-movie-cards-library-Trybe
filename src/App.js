a2750583a618a9ea91e212d80a9d24d94b3c4696
Vinicios <soicinivt@gmail.com>
2021-05-04 10:50:04 -0300

    Testes do header concluidos.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      {/*Implementação aqui. Remova essas duas linhas*/}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />
     </div>
   );
 }
