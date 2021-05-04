3d50b51e8f543a9494432a9b6eba46753215639a
Luiz Otavio <luiz.otavio273@gmail.com>
2021-05-04 20:02:36 -0300

    Requisito 3 - Cria um componente MovieList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Headers from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Headers />
+      <MovieList key={ movies.title } moviesList={ movies[0] } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,27 @@
-// implement MovieList component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { moviesList: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <section>
+        <h1>{ title }</h1>
+        <h2>{ subtitle }</h2>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+        <img src={ imagePath } alt={ title } />
+      </section>
+    );
+  }
+}
+
+// MovieList.protoType = {
+//   title: PropTypes.string.isRequireds,
+//   subtitle: PropTypes.string.isRequireds,
+//   storyline: PropTypes.string.isRequireds,
+//   rating: PropTypes.number.isRequireds,
+//   imagePath: PropTypes.string.isRequireds,
+// };
+
+export default MovieList;
