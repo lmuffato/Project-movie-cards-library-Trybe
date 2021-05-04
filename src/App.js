c69ac2191ed7b278f2124e24de61cd93f5754be9
Luiz Otavio <luiz.otavio273@gmail.com>
2021-05-04 16:59:52 -0300

    Requisitos 1 e 2 - Cria um componente e renderiza um texto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Headers from './components/Header';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Headers />
+    </div>
   );
 }
 
