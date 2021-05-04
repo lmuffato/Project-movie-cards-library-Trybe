a4ba4bbe39d9a804f1a462c09d965eb2a69fbbf9
Luiz Otavio <luiz.otavio273@gmail.com>
2021-05-04 16:38:02 -0300

    Iniciando o projeto Movie Cards Library

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
-    </div>
+    <div className="App"></div>
   );
 }
 
