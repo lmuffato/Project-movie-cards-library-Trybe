ed186e99302c03bfa177deb008954997884c5a45
biazidioti <bzferreira.bio@gmail.com>
2021-05-05 18:24:56 -0300

    Tentando fazer o requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+import movies from './data';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {
+          movies.map((movie) => (
+            <MovieList key={ movie.title } movie={ movie } />))
+        }
+      </div>
+    );
+  }
+}
+
+export default MovieList;
