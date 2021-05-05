e74fc81ecaa40f5b2cbb90db7e947d81a6d77cbb
biazidioti <bzferreira.bio@gmail.com>
2021-05-05 10:22:01 -0300

    Adiciona requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// Iniciando o projeto 🚀
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>Movie Cards Library</header>
+    );
+  }
+}
+
+export default Header;
