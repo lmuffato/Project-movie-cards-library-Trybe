d600662296d3bceeeaa1c6c8971050ebae38f5f5
Roberto Natividade <robertonnv@gmail.com>
2021-05-05 09:14:26 -0300

    Requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from 'react'
-import MovieCard from './MovieCard'
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
-    render() {
-        const { movies} = this.props;
-        
-        return (
-            <ul className='movie-list'>
-                {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-            </ul>
-        )
+  render() {
+     const { movies} = this.props;
+     return (
+        <ul className='movie-list'>
+          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        </ul>
+      )
     }
 }
 
 
-export default MovieList
+export default MovieList;
