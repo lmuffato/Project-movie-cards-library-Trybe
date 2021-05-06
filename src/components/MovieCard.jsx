ee71715b035abe8f07f204ee1ad04db10f840cf0
Renata Andrade <reapminas@gmail.com>
2021-05-06 12:48:10 -0300

    Requisitos 3, 4 e 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,46 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    // const keys = movies.map((movie) => Object.keys(movie).map((key) => ))
+    // console.log(keys)
+    // const filmes = movies.map((movie) => <p key={movie.title}>{movie.title}</p>
+    // )
+    // const { movie: oneMovie } = this.props;
+    const { movie } = this.props;
+    console.log(movie);
+    return (
+      <section>
+        <p>{movie.title}</p>
+        <p>{movie.subtitle}</p>
+        <p>{movie.storyline}</p>
+        <p>
+          {movie.rating}
+          {movie.ratingThree}
+        </p>
+        <p>{movie.imagePath}</p>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+    ratingThree: PropTypes.number,
+    imagePath: PropTypes.string.isRequired,
+  }),
+};
+
+MovieCard.defaultProps = {
+  movie: PropTypes.shape({
+    rating: 0,
+    ratingThree: 0,
+  }),
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,29 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    console.log(this.props);
+    const { movies } = this.props;
+    console.log(movies);
+    return (
+      <section>
+        {/* Como usar keys para identificar os elementos: https://reactjs.org/docs/lists-and-keys.html#keys */}
+        { movies.map((movie) => (
+          <li key={ movie.title }>
+            <MovieCard movie={ movie } />
+          </li>
+        ))}
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
+// Referências:
+// Como usar o PropTypes para corrigir erros do lint: https://stackoverflow.com/questions/38684925/react-eslint-error-missing-in-props-validation
