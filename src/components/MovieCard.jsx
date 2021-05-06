af90f27e775f8512352f488ec530b21359b2bc2e
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-05-05 22:54:37 -0300

    inclusao do requisito 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
-// implement MovieCard component here
+import React from 'react';
+
+function MovieCards() {
+//   const {} = this.props.movie;
+  return (
+    <section>
+      teste
+    </section>
+  );
+}
+
+export default MovieCards;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import MovieCards from './MovieCard';
 
 function MovieList() {
   return (
     <div>
       <ul>
-        teste
+        <MovieCards />
       </ul>
     </div>
   );
