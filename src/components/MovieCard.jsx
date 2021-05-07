0b6af8d7239881b1b4de2d372e1e6e2831255adb
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-05-06 20:25:11 -0400

    add req[3 -> 14]

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+import './MovieCard.css';
+
+export default function MovieCard(prop) {
+  const { movie } = prop;
+  const { imagePath, title, subtitle, storyline, rating } = movie;
+  return (
+    <div className="MovieCard">
+      <img src={ imagePath } alt={ `Imagem de ${title}` } />
+      <div className="textContent">
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <p className="rating">{ rating }</p>
+      </div>
+    </div>
+  );
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+import './MovieList.css';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <main>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </main>
+    );
+  }
+}
+
+export default MovieList;
