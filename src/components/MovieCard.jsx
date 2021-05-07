4acb73ae388ef09c858761cdc82a24f359caaa5c
Tiago Santos <tiago881@gmail.com>
2021-05-07 12:18:28 -0300

    requisitos, 3,4 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,7 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+export default class MovieCard extends Component {
+  render() {
+    return 'teste';
+  }
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+export default class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
+        ))}
+      </section>
+    );
+  }
+}
