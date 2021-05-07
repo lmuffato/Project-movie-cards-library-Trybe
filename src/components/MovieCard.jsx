2112df9452dd399431476119a75b4fcb89bb5676
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-05-06 23:06:43 -0300

    inclusao do requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath, subtitle } = movie;
+    const { title, imagePath, subtitle, storyline } = movie;
     return (
       <section>
         <img src={ imagePath } alt="" />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </section>
     );
   }
@@ -20,6 +21,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     imagePath: PropTypes.string,
     subtitle: PropTypes.string,
+    storyline: PropTypes.string,
   }).isRequired,
 };
 
