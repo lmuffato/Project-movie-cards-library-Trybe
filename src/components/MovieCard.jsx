683d053adb22e5fcbd0522c63108c6a4cf2484fa
Tiago Santos <tiago881@gmail.com>
2021-05-10 19:07:31 -0300

    8 - Renderize o título do filme dentro de uma tag `h4`

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 export default class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    return <img src={movie.imagePath} alt={movie.title} />;
+    return (
+      <div>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        ,
+        <h4>{movie.title}</h4>
+      </div>
+    );
   }
 }
