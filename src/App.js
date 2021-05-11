25b168da96b1989406286a93fc6bdb27110fde61
jlfagundes <jlfagundes@gmail.com>
2021-05-11 16:52:35 -0300

    Requeriments three, four and five done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import Movies from './data';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
       <Header />
+      <MovieList movies={ Movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieList(props) {
+  const { movies } = props;
+  return (
+    movies.map((movie) => (
+      <MovieCard key={ movie.title } />
+    ))
+  );
+}
+
+// class MovieList extends React.Component {
+//   render() {
+//     return (
+//       <h1>Movie List</h1>
+//     );
+//   }
+// }
+
+export default MovieList;
