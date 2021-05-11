dea5e61c5bd2d87852e4d0e2eeae8113a12f076d
jlfagundes <jlfagundes@gmail.com>
2021-05-11 16:11:56 -0300

    Requeriment one and two done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,15 @@
 // implement Header component here
 // Iniciando o projeto
+// Código baseado na solução encontrado por Edmilson Bernardo
+
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Movie Cards Library</h1>
+        </header>
+    )
+}
+
+export default Header;
