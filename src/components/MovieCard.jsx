a3fa1f8a855a2b86457a3303dbbf5a48495639ff
Apoc35 <alexv_silveira@outlook.com>
2021-05-12 20:03:43 -0300

    Requisitos finais.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { shape, string } from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <div>
-        <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <div className="movie-card-rating ">
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: shape({
+    title: string,
+    subtitle: string,
+    storyline: string,
+    imagePath: string,
+  }),
+}.isRequired;
 
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,15 @@
-// implement Rating component here
+import React from 'react';
+import { number } from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return <span className="rating">{ rating }</span>;
+  }
+}
+
+Rating.propTypes = {
+  rating: number,
+}.isRequired;
+
+export default Rating;
