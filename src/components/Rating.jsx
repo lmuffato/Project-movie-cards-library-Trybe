f893f45722ab3ba09efdb695d54becb6688f32b1
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-05-15 22:03:04 -0300

    ate requisito 14

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class Rating extends Component {}
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div>{rating}</div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.oneOfType(PropTypes.number, PropTypes.string).isRequired,
+};
 
 export default Rating;
