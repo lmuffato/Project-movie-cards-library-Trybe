491df81ce89864a80353fd59a7d1aa5f760ecea1
pauloeliezerg <pauloeliezerg@gmail.com>
2021-05-17 16:35:57 -0300

    Added Header initial content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 function Header() {
   return (
     <div>
-      Header
+      <header>
+        <h1>
+          Movie Cards Library
+        </h1>
+      </header>
     </div>
   );
 }
