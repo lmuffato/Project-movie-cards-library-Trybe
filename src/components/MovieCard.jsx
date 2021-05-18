2b4f1728119e52628c7748bd4265ceb826fa8eb8
pauloeliezerg <pauloeliezerg@gmail.com>
2021-05-17 21:47:55 -0300

    Added proptypes and imagePath

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { imagePath } = movie;
     return (
       <div>
-        MovieCard
+        <img src={ imagePath } alt="" />
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      imagePath: PropTypes.string,
+      rating: PropTypes.number,
+    }),
+  ).isRequired,
+};
 
 export default MovieCard;
