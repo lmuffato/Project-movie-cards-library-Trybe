019d524e4c72276d417b84f4da38b524f9fc4788
pauloeliezerg <pauloeliezerg@gmail.com>
2021-05-17 21:54:55 -0300

    Added subtitle on MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,12 +4,15 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath } = movie;
+    const { title, subtitle, imagePath } = movie;
     return (
       <div>
         <h4>
           { title }
         </h4>
+        <h5>
+          { subtitle }
+        </h5>
         <img src={ imagePath } alt="" />
       </div>
     );
