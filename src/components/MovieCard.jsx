527e9e61e39bba5b7d02bbd595e361be65c89f23
pauloeliezerg <pauloeliezerg@gmail.com>
2021-05-17 21:56:10 -0300

    Added storyline on MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
       <div>
         <h4>
@@ -14,6 +14,9 @@ class MovieCard extends React.Component {
           { subtitle }
         </h5>
         <img src={ imagePath } alt="" />
+        <p>
+          { storyline }
+        </p>
       </div>
     );
   }
