d235c7d83936c8e11a762283c272dd82380579b7
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-05-19 18:47:57 -0300

    corrige o lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class Hearder extends React.Component {
-    render() { 
-      return (
-        <header>
-          <h1>Movie Cards Library</h1>
-        </header>
-      )
-    } 
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  } 
 }
 
 export default Hearder
