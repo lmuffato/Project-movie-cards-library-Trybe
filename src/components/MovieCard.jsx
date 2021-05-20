8e96b71f0038996ab8fcd2cd7ebb95861971342d
MZTBTG <mztbtg@hotmail.com>
2021-05-20 18:40:35 -0300

    Exercício 6: Concluído

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function MovieCard() {
+function MovieCard({ movie }) {
   return (
-    <div>
-      asdf
+    <div className="movie-card">
+      <img src={ movie.imagePath } alt={ `foto ${movie.title}` } />
+      <h2>{ movie.title }</h2>
+      <h3>{ movie.subtitle }</h3>
+      <p>{ movie.storyline }</p>
     </div>
   );
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
