b59a70364bb4bc8823693979f1ed1fc47f1ee8ad
Wendrick <wendrick.b.c1@gmail.com>
2021-05-20 23:26:22 -0300

    Adicionei alguns requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,6 +11,8 @@ class MovieCard extends React.Component {
         <h4> { title } </h4>
         <h5> { subtitle } </h5>
         <p> { storyline } </p>
+        <Rating rating={ rating } />
+        <img src={ imagePath } alt={ title } />
       </div>
     );
   }
