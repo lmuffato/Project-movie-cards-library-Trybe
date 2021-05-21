f0036767a518cf10531b8c1f4bc0786008c79a95
akirakiraa <andretaki2@gmail.com>
2021-05-21 01:37:54 -0300

    importing stuff / movie list test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+// import MovieCard from './components/MovieCard';
+// import Rating from './components/Rating';
 
 function App() {
-  return <Header />;
+  return (
+    <main>
+      <Header />
+      <MovieList />
+    </main>
+  );
 }
 
 export default App;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <section>
+        <h1>texto</h1>
+      </section>
+    );
+  }
+}
+
+export default MovieList;
