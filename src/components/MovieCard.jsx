0ba36121bc7802441e85aef2dcec798aaf712c1f
Wendrick <wendrick.b.c1@gmail.com>
2021-05-20 22:25:29 -0300

    Pausa peguei e botei o title no h4 e o sub no h5 vou passar o rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import MoviesData from '../data';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const {title, subtitle, storyline, rating, imagePath} = movie;
     return (
-      <p>
-        { MoviesData }
-      </p>
+      <div>
+        <h4> { title } </h4>
+        <h5> { subtitle } </h5>
+        <p> { storyline } </p>
+      </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieCard;
