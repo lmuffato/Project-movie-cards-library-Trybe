6a2a6e3023b7e39e8c7ba29bb3e4b321a83fb56f
akirakiraa <andretaki2@gmail.com>
2021-05-21 04:23:19 -0300

    Rate me

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-    // const { movie } = this.props;
+    const { movies } = this.props;
     return (
       <section>
-        <h1>Movie List</h1>
-        <MovieCard />
+        {movies.map((movie) => (
+          <MovieCard movie={ movie } key={ movie.title } />))}
       </section>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
