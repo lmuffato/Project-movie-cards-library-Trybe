9d7839c8fad0c4f8dd035887b84475b2d8a01c85
Wendrick <wendrick.b.c1@gmail.com>
2021-05-20 23:41:27 -0300

    Correçao part 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,14 +8,14 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <div>
-        <h4> 
-          { title } 
+        <h4>
+          { title }
         </h4>
-        <h5> 
-          { subtitle } 
+        <h5>
+          { subtitle }
         </h5>
-        <p> 
-          { storyline } 
+        <p>
+          { storyline }
         </p>
         <Rating rating={ rating } />
         <img src={ imagePath } alt={ title } />
