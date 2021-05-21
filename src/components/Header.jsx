7c1fdb57f06db92b228d6752b71e0a50af427346
akirakiraa <andretaki2@gmail.com>
2021-05-21 00:12:02 -0300

    slow brow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
-// implement Header component here ok
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
+
+// todo o esqueleto do paragrafo acima foi escrito
+// com o rcc da extensão react code snippets
